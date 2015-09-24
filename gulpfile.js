fd14a26dea0c0e0986a453c7330ceae2fd21ef78
Blackbaud-RobertTester <robert.tester@blackbaud.com>
2015-09-24 10:50:21 -0500

    Minor gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     jshint = require('gulp-jshint'),
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat');
 
 gulp.task('default', ['jshint']);
 
@@ -22,22 +23,23 @@ gulp.task('build-css', function () {
 
 //NOTE: In order to set the environment type pass in <b>--type production</b> to the gulp command
 gulp.task('deploy-sky', function() {
-    if(gutil.env.type == 'production') {
-        gutil.log('production sky');
-    }
-    else {
-        //Copy the sky dependencies including AngularJS
-        gulp.src(['bower_components/bb-sky/dist/js/libs.min.js*']).pipe(gulp.dest('app/assets/js'));
-        gulp.src('bower_components/bb-sky/dist/css/libs.css*').pipe(gulp.dest('app/assets/css'));
+    //Copy the sky dependencies including AngularJS
+    gulp.src(['bower_components/bb-sky/dist/js/libs.min.js*']).pipe(gulp.dest('app/assets/js'));
+    gulp.src('bower_components/bb-sky/dist/css/libs.css*').pipe(gulp.dest('app/assets/css'));
 
-        //Copy sky library
-        gulp.src(['bower_components/bb-sky/dist/js/sky.min.js*']).pipe(gulp.dest('app/assets/js'));
-        gulp.src('bower_components/bb-sky/dist/css/sky.css*').pipe(gulp.dest('app/assets/css'));
+    //Copy sky library
+    gulp.src(['bower_components/bb-sky/dist/js/sky.min.js*']).pipe(gulp.dest('app/assets/js'));
+    gulp.src('bower_components/bb-sky/dist/css/sky.css*').pipe(gulp.dest('app/assets/css'));
 
-        //Copy fonts
-        gulp.src('bower_components/bb-sky/dist/css/fonts/**').pipe(gulp.dest('app/assets/css/fonts'));
-    }
+    //Copy fonts
+    gulp.src('bower_components/bb-sky/dist/css/fonts/**').pipe(gulp.dest('app/assets/css/fonts'));
 });
 
 //We need to build any external dependencies that we have
-
+gulp.task('deploy-bluemoon-libraries', function () {
+    gulp.src(['bower_components/restangular/dist/restangular.js', 'bower_components/underscore/underscore.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('bluemoon-libs.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/assets/js'));
+});
